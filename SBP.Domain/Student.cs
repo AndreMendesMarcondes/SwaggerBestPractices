c675a8ce86ea380a20c1c0acb21a9a2d28259a63
André Mendes Marcondes <andre.marcondes@betalearning.com.br>
2022-06-27 14:38:02 -0300

    criacao de dominio

--- SBP.Domain/Student.cs
+++ SBP.Domain/Student.cs
@@ -2,6 +2,11 @@
 {
     public class Student
     {
-
+        public Guid StudendId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DateBirth { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CPF { get; set; }
     }
 }
